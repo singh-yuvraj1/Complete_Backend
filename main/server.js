9645f9d3db14a983c2504f704f3004f620166cb3
singh-yuvraj1 <yuvraj.singh.95928@gmail.com>
2026-08-30 15:43:24 +0530

    Working with rest api and postman to create a notes app in express

--- main/server.js
+++ main/server.js
@@ -1,5 +1,6 @@
 const app = require("./src/app")
 
-app.listen(3000, () => {
-    console.log("Server is running on port no 3000")
+
+app.listen(3000, ()=>{
+    console.log("Server is running on port no 3000");
 })
--- main/src/app.js
+++ main/src/app.js
@@ -1,17 +1,79 @@
 const express = require("express")
 
 const app = express()
+const notes = []
 
 app.use(express.json())
 
-const notes = []
 
-app.post('/notes' , (req , res)=>{
-    console.log(req.body)
-
-        notes.push(req.body)
-
-    res.send("Note added successfully")
+app.post("/notes" , (req , res)=>{
+    notes.push(req.body)
+    res.status(201).json({
+        message : "Note Created successfully"
+    })
 })
 
-module.exports = app
+
+app.get("/notes" , (req , res)=>{
+    res.status(200).json({
+        message : "Notes Fetched Successfully",
+        notes : notes
+    })
+})
+
+
+app.delete("/notes/:index" , (req, res)=>{
+    const index = req.params.index
+
+    delete notes[index]
+
+    res.status(200).json({
+        message: "Note Deleted Successfully"
+    })
+})
+
+
+
+module.exports =  app
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require("express")
+
+// const app = express()
+
+// app.use(express.json())
+
+// const notes = []
+
+// app.post('/notes' , (req , res)=>{
+//     console.log(req.body)
+
+//         notes.push(req.body)
+
+//     res.send("Note added successfully")
+// })
+
+// module.exports = app
